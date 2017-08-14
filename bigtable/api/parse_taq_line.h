4aec2351e289f194628e188b103800a08c6e829a
Carlos O'Ryan <coryan@google.com>
2017-08-14 15:55:44 -0400

    Document the format of the TAQ file.

--- bigtable/api/parse_taq_line.h
+++ bigtable/api/parse_taq_line.h
@@ -21,6 +21,20 @@
 
 namespace bigtable_api_samples {
 // Parse a line from a TAQ file and convert it to a quote.
+//
+// TAQ files are delimiter (using '|' as the delimiter) separated text
+// files, using this format:
+//
+// timestamp|exchange|ticker|bid price|bid qty|offer price|offer qty|...
+// 093000123456789|K|GOOG|800.00|100|900.00|200|...
+// ...
+// END|20161024|78721395|||||||||||||||||||||||||
+//
+// The first line is a header, it defines the fields, each line
+// contains all the data for a quote, in this example we are only
+// interested in the first few fields, the last line is indicated by
+// the 'END' marker, it contains the date (timestamps are relative to
+// midnight on this date), and the total number of lines.
 Quote parse_taq_line(int lineno, std::string const& line);
 } // namespace bigtable_api_samples
 
