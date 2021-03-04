31458f4366fa05fae987689b898c9d8e0ef8c1f4
Carlos O'Ryan <coryan@google.com>
2021-03-04 15:27:41 +0000

    Refactor for easier reading

--- gcs-parallel-download/download.cc
+++ gcs-parallel-download/download.cc
@@ -40,6 +40,51 @@ po::variables_map parse_command_line(int argc, char* argv[]);
 std::string format_size(std::int64_t size);
 int check_system_call(std::string const& name, int result);
 
+std::vector<std::int64_t> compute_slices(std::int64_t object_size,
+                                         po::variables_map const& vm) {
+  auto const minimum_slice_size = vm["minimum-slice-size"].as<std::int64_t>();
+  auto const thread_count = vm["thread-count"].as<int>();
+
+  std::vector<std::int64_t> result;
+  auto const thread_slice = object_size / thread_count;
+  if (thread_slice >= minimum_slice_size) {
+    std::fill_n(std::back_inserter(result), thread_count, thread_slice);
+    // If the object size is not a multiple of the thread count we may need
+    // to spread any excess bytes across the slices.
+    auto loc = result.rbegin();
+    for (auto e = object_size % thread_count; e != 0; --e, ++loc) ++(*loc);
+    return result;
+  }
+  for (; object_size > 0; object_size -= minimum_slice_size) {
+    result.push_back(std::min(minimum_slice_size, object_size));
+  }
+  return result;
+}
+
+std::string task(std::int64_t offset, std::int64_t length,
+                 std::string const& bucket, std::string const& object, int fd) {
+  auto client = gcs::Client::CreateDefaultClient().value();
+  auto is = client.ReadObject(bucket, object,
+                              gcs::ReadRange(offset, offset + length));
+
+  std::vector<char> buffer(1024 * 1024L);
+  std::int64_t count = 0;
+  std::int64_t write_offset = offset;
+  do {
+    is.read(buffer.data(), buffer.size());
+    if (is.bad()) break;
+    count += is.gcount();
+    check_system_call("pwrite()",
+                      ::pwrite(fd, buffer.data(), is.gcount(), write_offset));
+    write_offset += is.gcount();
+  } while (not is.eof());
+  return fmt::format("Download range [{}, {}] got {}/{} bytes", offset,
+                     offset + length, count, length);
+}
+
+// Get the size and crc32c checksum of a file
+std::pair<std::int64_t, std::string> file_info(std::string const& filename);
+
 auto constexpr kKiB = std::int64_t(1024);
 auto constexpr kMiB = 1024 * kKiB;
 auto constexpr kGiB = 1024 * kMiB;
@@ -49,7 +94,6 @@ auto constexpr kPiB = 1024 * kTiB;
 
 int main(int argc, char* argv[]) try {
   auto vm = parse_command_line(argc, argv);
-
   auto const bucket = vm["bucket"].as<std::string>();
   auto const object = vm["object"].as<std::string>();
   auto const destination = vm["destination"].as<std::string>();
@@ -57,54 +101,13 @@ int main(int argc, char* argv[]) try {
   auto client = gcs::Client::CreateDefaultClient().value();
   auto metadata = client.GetObjectMetadata(bucket, object).value();
 
-  auto compute_slices = [&vm](std::int64_t object_size) {
-    auto const minimum_slice_size = vm["minimum-slice-size"].as<std::int64_t>();
-    auto const thread_count = vm["thread-count"].as<int>();
-
-    std::vector<std::int64_t> result;
-    auto const thread_slice = object_size / thread_count;
-    if (thread_slice >= minimum_slice_size) {
-      std::fill_n(std::back_inserter(result), thread_count, thread_slice);
-      // If the object size is not a multiple of the thread count we may need
-      // to spread any excess bytes across the slices.
-      auto loc = result.rbegin();
-      for (auto e = object_size % thread_count; e != 0; --e, ++loc) ++(*loc);
-      return result;
-    }
-    for (; object_size > 0; object_size -= minimum_slice_size) {
-      result.push_back(std::min(minimum_slice_size, object_size));
-    }
-    return result;
-  };
-
-  auto slices = compute_slices(metadata.size());
+  auto slices = compute_slices(metadata.size(), vm);
 
   std::cout << "Downloading " << object << " from bucket " << bucket
             << " to file " << destination << "\n";
   std::cout << "This object size is approximately "
             << format_size(metadata.size()) << ". It will be downloaded in "
             << slices.size() << " slices." << std::endl;
-
-  auto task = [](std::int64_t offset, std::int64_t length,
-                 std::string const& bucket, std::string const& object, int fd) {
-    auto client = gcs::Client::CreateDefaultClient().value();
-    auto is = client.ReadObject(bucket, object,
-                                gcs::ReadRange(offset, offset + length));
-
-    std::vector<char> buffer(1024 * 1024L);
-    std::int64_t count = 0;
-    std::int64_t write_offset = offset;
-    do {
-      is.read(buffer.data(), buffer.size());
-      if (is.bad()) break;
-      count += is.gcount();
-      check_system_call("pwrite()",
-                        ::pwrite(fd, buffer.data(), is.gcount(), write_offset));
-      write_offset += is.gcount();
-    } while (not is.eof());
-    return fmt::format("Download range [{}, {}] got {}/{} bytes", offset,
-                       offset + length, count, length);
-  };
 
   auto const start = std::chrono::steady_clock::now();
   auto constexpr kOpenFlags = O_CREAT | O_TRUNC | O_WRONLY;
@@ -135,27 +138,7 @@ int main(int argc, char* argv[]) try {
   std::cout << "Download completed in " << elapsed_ms.count() << "ms\n"
             << "Effective bandwidth " << effective_bandwidth_MiBs << " MiB/s\n";
 
-  auto downloaded_file_info = [](std::string const& filename) {
-    std::ifstream is(filename);
-    std::vector<char> buffer(1024 * 1024L);
-    std::uint32_t crc32c = 0;
-    std::int64_t size = 0;
-    do {
-      is.read(buffer.data(), buffer.size());
-      if (is.bad()) break;
-      crc32c = crc32c::Extend(
-          crc32c, reinterpret_cast<std::uint8_t*>(buffer.data()), is.gcount());
-      size += is.gcount();
-    } while (not is.eof());
-
-    static_assert(std::numeric_limits<unsigned char>::digits == 8,
-                  "This program assumes an 8-bit char");
-    boost::endian::big_uint32_buf_at buf(crc32c);
-    return std::make_pair(size, cppcodec::base64_rfc4648::encode(std::string(
-                                    buf.data(), buf.data() + sizeof(buf))));
-  };
-
-  auto [size, crc32c] = downloaded_file_info(destination);
+  auto [size, crc32c] = file_info(destination);
   if (size != metadata.size()) {
     std::cout << "Downloaded file size mismatch, expected=" << metadata.size()
               << ", got=" << size << std::endl;
@@ -292,6 +275,26 @@ int check_system_call(std::string const& name, int result) {
   throw std::runtime_error(
       fmt::format("Error in {}() - return value={}, error=[{}] {}", name,
                   result, err, strerror(err)));
+}
+
+std::pair<std::int64_t, std::string> file_info(std::string const& filename) {
+  std::ifstream is(filename);
+  std::vector<char> buffer(1024 * 1024L);
+  std::uint32_t crc32c = 0;
+  std::int64_t size = 0;
+  do {
+    is.read(buffer.data(), buffer.size());
+    if (is.bad()) break;
+    crc32c = crc32c::Extend(
+        crc32c, reinterpret_cast<std::uint8_t*>(buffer.data()), is.gcount());
+    size += is.gcount();
+  } while (not is.eof());
+
+  static_assert(std::numeric_limits<unsigned char>::digits == 8,
+                "This program assumes an 8-bit char");
+  boost::endian::big_uint32_buf_at buf(crc32c);
+  return {size, cppcodec::base64_rfc4648::encode(
+                    std::string(buf.data(), buf.data() + sizeof(buf)))};
 }
 
 }  // namespace
