23014f9e1622a5ab6353a570d974030016fde559
Carlos O'Ryan <coryan@users.noreply.github.com>
2021-08-27 13:42:52 -0700

    feat: implement first working indexer (#140)

---
+++ getting-started/gcs_indexing.cc
@@ -0,0 +1,175 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gcs_indexing.h"
+#include <nlohmann/json.hpp>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+namespace google::cloud::cpp_samples {
+
+namespace gcs = ::google::cloud::storage;
+namespace spanner = ::google::cloud::spanner;
+
+nlohmann::json LogFormat(std::string const& sev, std::string const& msg) {
+  return nlohmann::json{{"severity", sev}, {"message", msg}}.dump();
+}
+
+void LogError(std::string const& msg) {
+  std::cerr << LogFormat("error", msg) << "\n";
+}
+
+using GetField = std::function<spanner::Value(gcs::ObjectMetadata const&)>;
+
+auto const& Columns() {
+  static auto const columns = [] {
+    auto field = [](auto name, auto functor) {
+      return std::pair<std::string const, GetField>(
+          std::move(name),
+          [f = std::move(functor)](gcs::ObjectMetadata const& o) {
+            return spanner::Value(f(o));
+          });
+    };
+
+    auto custom = [](auto name, auto functor) {
+      return std::pair<std::string const, GetField>(
+          std::move(name), [f = std::move(functor)](
+                               gcs::ObjectMetadata const& o) { return f(o); });
+    };
+
+    auto optional_string = [](auto name, auto functor) {
+      return std::pair<std::string const, GetField>(
+          std::move(name),
+          [f = std::move(functor)](gcs::ObjectMetadata const& o) {
+            auto s = f(o);
+            if (s.empty()) return spanner::Value(absl::optional<std::string>());
+            return spanner::Value(std::move(s));
+          });
+    };
+
+    auto timestamp = [](auto name, auto functor) {
+      return std::pair<std::string const, GetField>(
+          std::move(name),
+          [f = std::move(functor)](gcs::ObjectMetadata const& o) {
+            auto tp = f(o);
+            if (tp == std::chrono::system_clock::time_point{}) {
+              return spanner::Value(absl::optional<spanner::Timestamp>());
+            }
+            auto ts = spanner::MakeTimestamp(tp).value();
+            return spanner::Value(ts);
+          });
+    };
+
+    return std::map<std::string, GetField>({
+        field("name", [](auto const& o) { return o.name(); }),
+        field("bucket", [](auto const& o) { return o.bucket(); }),
+        field("generation", [](auto const& o) { return o.generation(); }),
+        field("metageneration",
+              [](auto const& o) { return o.metageneration(); }),
+        timestamp("timeCreated",
+                  [](auto const& o) { return o.time_created(); }),
+        timestamp("updated", [](auto const& o) { return o.updated(); }),
+        timestamp("timeDeleted",
+                  [](auto const& o) { return o.time_deleted(); }),
+        timestamp("customTime", [](auto const& o) { return o.custom_time(); }),
+        field("temporaryHold",
+              [](auto const& o) { return o.temporary_hold(); }),
+        field("eventBasedHold",
+              [](auto const& o) { return o.event_based_hold(); }),
+        timestamp("retentionExpirationTime",
+                  [](auto const& o) { return o.retention_expiration_time(); }),
+        field("storageClass", [](auto const& o) { return o.storage_class(); }),
+        timestamp("timeStorageClassUpdated",
+                  [](auto const& o) { return o.time_storage_class_updated(); }),
+        field(
+            "size",
+            [](auto const& o) { return static_cast<std::int64_t>(o.size()); }),
+        field("crc32c", [](auto const& o) { return o.crc32c(); }),
+        optional_string("md5Hash", [](auto const& o) { return o.md5_hash(); }),
+        optional_string("contentType",
+                        [](auto const& o) { return o.content_type(); }),
+        optional_string("contentEncoding",
+                        [](auto const& o) { return o.content_encoding(); }),
+        optional_string("contentDisposition",
+                        [](auto const& o) { return o.content_disposition(); }),
+        optional_string("contentLanguage",
+                        [](auto const& o) { return o.content_language(); }),
+        optional_string("cacheControl",
+                        [](auto const& o) { return o.cache_control(); }),
+
+        field("metadata",
+              [](auto const& o) {
+                nlohmann::json json{};
+                for (auto const& [k, v] : o.metadata()) json[k] = v;
+                return json.dump();
+              }),
+        custom("owner",
+               [](auto const& o) {
+                 if (!o.has_owner()) {
+                   return spanner::Value(absl::optional<std::string>());
+                 }
+                 return spanner::Value(nlohmann::json{
+                     {"entity", o.owner().entity},
+                     {"entityId",
+                      o.owner().entity_id}}.dump());
+               }),
+        field("componentCount",
+              [](auto const& o) { return o.component_count(); }),
+        optional_string("etag", [](auto const& o) { return o.etag(); }),
+        custom("customerEncryption",
+               [](auto const& o) {
+                 if (!o.has_customer_encryption()) {
+                   return spanner::Value(absl::optional<std::string>());
+                 }
+                 return spanner::Value(nlohmann::json{
+                     {"encryptionAlgorithm",
+                      o.customer_encryption().encryption_algorithm},
+                     {"keySha256", o.customer_encryption().key_sha256}}
+                                           .dump());
+               }),
+        optional_string("kmsKeyName",
+                        [](auto const& o) { return o.kms_key_name(); }),
+    });
+  }();
+  return columns;
+}
+
+auto Names() {
+  static auto const names = [] {
+    auto columns = Columns();
+    std::vector<std::string> names(columns.size());
+    std::transform(columns.begin(), columns.end(), names.begin(),
+                   [](auto p) { return p.first; });
+    return names;
+  }();
+  return names;
+}
+
+spanner::Mutation UpdateObjectMetadata(gcs::ObjectMetadata const& object) {
+  auto const& columns = Columns();
+  std::vector<spanner::Value> values(columns.size());
+  std::transform(columns.begin(), columns.end(), values.begin(),
+                 [&object](auto const& p) {
+                   auto const& [name, to_value] = p;
+                   return to_value(object);
+                 });
+  return spanner::InsertOrUpdateMutationBuilder("gcs_objects", Names())
+      .AddRow(std::move(values))
+      .Build();
+}
+
+}  // namespace google::cloud::cpp_samples
---
+++ getting-started/index_gcs_prefix.cc
@@ -0,0 +1,163 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gcs_indexing.h"
+#include <google/cloud/functions/cloud_event.h>
+#include <google/cloud/pubsub/publisher.h>
+#include <google/cloud/spanner/client.h>
+#include <google/cloud/storage/client.h>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace {
+
+namespace gcf = ::google::cloud::functions;
+namespace gcs = ::google::cloud::storage;
+namespace pubsub = ::google::cloud::pubsub;
+namespace spanner = ::google::cloud::spanner;
+using google::cloud::cpp_samples::LogError;
+using google::cloud::cpp_samples::UpdateObjectMetadata;
+
+std::string GetEnv(char const* var) {
+  auto const* value = std::getenv(var);
+  if (value == nullptr) {
+    throw std::runtime_error("Environment variable " + std::string(var) +
+                             " is not set");
+  }
+  return value;
+}
+
+pubsub::Publisher GetPublisher() {
+  static auto const publisher = [&] {
+    auto topic =
+        pubsub::Topic(GetEnv("GOOGLE_CLOUD_PROJECT"), GetEnv("TOPIC_ID"));
+    return pubsub::Publisher(
+        pubsub::MakePublisherConnection(std::move(topic), {}));
+  }();
+  return publisher;
+}
+
+spanner::Client GetSpannerClient() {
+  static auto const client = [&] {
+    auto database = spanner::Database(GetEnv("GOOGLE_CLOUD_PROJECT"),
+                                      GetEnv("SPANNER_INSTANCE"),
+                                      GetEnv("SPANNER_DATABASE"));
+    return spanner::Client(spanner::MakeConnection(std::move(database)));
+  }();
+  return client;
+}
+
+void ThrowIfNotOkay(std::string const& context,
+                    google::cloud::Status const& status) {
+  if (status.ok()) return;
+  std::ostringstream os;
+  os << "error while " << context << " status=" << status;
+  throw std::runtime_error(std::move(os).str());
+}
+
+}  // namespace
+
+void IndexGcsPrefix(gcf::CloudEvent event) {  // NOLINT
+  // We only have 10 seconds to handle this event. If there is a lot of data in
+  // a prefix we will need to stop and issue a new event to continue.
+  auto const deadline =
+      std::chrono::steady_clock::now() + std::chrono::seconds(7);
+
+  if (event.data_content_type().value_or("") != "application/json") {
+    return LogError("expected application/json data");
+  }
+  auto const payload = nlohmann::json::parse(event.data().value_or("{}"));
+  if (!payload.contains("message")) {
+    return LogError("missing embedded Pub/Sub message");
+  }
+  auto const& message = payload["message"];
+  if (!message.contains("attributes")) {
+    return LogError("missing Pub/Sub attributes");
+  }
+  auto const& attributes = message["attributes"];
+  if (!attributes.contains("bucket")) {
+    return LogError("missing 'bucket' attribute in Pub/Sub message");
+  }
+  auto const bucket = attributes.value("bucket", "");
+  auto const prefix = [&attributes]() {
+    if (!attributes.contains("prefix")) return gcs::Prefix();
+    return gcs::Prefix(attributes.value("prefix", ""));
+  }();
+  auto const start = [&attributes]() {
+    if (!attributes.contains("start")) return gcs::StartOffset();
+    return gcs::StartOffset(attributes.value("start", ""));
+  }();
+
+  auto client = gcs::Client::CreateDefaultClient().value();
+  auto publisher = GetPublisher();
+
+  int mutation_count = 0;
+  std::vector<google::cloud::future<google::cloud::Status>> pending;
+  for (auto const& entry : client.ListObjectsAndPrefixes(bucket, prefix, start,
+                                                         gcs::Delimiter("/"))) {
+    ThrowIfNotOkay("listing bucket " + bucket, entry.status());
+    auto const now = std::chrono::steady_clock::now();
+    if (now > deadline) {
+      struct SplitPoint {
+        std::string operator()(std::string const& s) { return s; }
+        std::string operator()(gcs::ObjectMetadata const& o) {
+          return o.name();
+        }
+      };
+      std::string start = absl::visit(SplitPoint{}, *entry);
+      auto builder = pubsub::MessageBuilder{}
+                         .InsertAttribute("bucket", bucket)
+                         .InsertAttribute("start", std::move(start));
+      if (prefix.has_value()) {
+        builder.InsertAttribute("prefix", prefix.value());
+      }
+      pending.push_back(
+          publisher.Publish(std::move(builder).Build()).then([](auto f) {
+            return f.get().status();
+          }));
+      break;
+    }
+
+    if (absl::holds_alternative<std::string>(*entry)) {
+      pending.push_back(
+          publisher
+              .Publish(
+                  pubsub::MessageBuilder{}
+                      .InsertAttribute("bucket", bucket)
+                      .InsertAttribute("prefix", absl::get<std::string>(*entry))
+                      .Build())
+              .then([](auto f) { return f.get().status(); }));
+    } else {
+      auto const& object = absl::get<gcs::ObjectMetadata>(*entry);
+      auto update = UpdateObjectMetadata(object);
+      GetSpannerClient()
+          .Commit(
+              [m = std::move(update)](auto) { return spanner::Mutations{m}; })
+          .value();
+      ++mutation_count;
+    }
+  }
+  publisher.Flush();
+  google::cloud::Status status;
+  for (auto& p : pending) {
+    auto publish_status = p.get();
+    if (publish_status.ok()) continue;
+    status = std::move(publish_status);
+  }
+  ThrowIfNotOkay("publishing one or more messages", status);
+  std::cout << "DEBUG inserted " << mutation_count << " rows and sent "
+            << pending.size() << " messages\n";
+}
