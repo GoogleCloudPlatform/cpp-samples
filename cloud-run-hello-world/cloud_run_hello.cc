f48022f7f6c127eec98dd9f88aff88570eb716dd
Carlos O'Ryan <coryan@google.com>
2022-05-11 12:56:36 +0000

    Fix formatting

--- cloud-run-hello-world/cloud_run_hello.cc
+++ cloud-run-hello-world/cloud_run_hello.cc
@@ -21,15 +21,15 @@ namespace gcf = ::google::cloud::functions;
 
 auto hello_world_http() {
   return gcf::MakeFunction([](gcf::HttpRequest const& /*request*/) {
-      std::string greeting = "Hello ";
-      auto const* target = std::getenv("TARGET");
-      greeting += target == nullptr ? "World" : target;
-      greeting += "\n";
+    std::string greeting = "Hello ";
+    auto const* target = std::getenv("TARGET");
+    greeting += target == nullptr ? "World" : target;
+    greeting += "\n";
 
-      return gcf::HttpResponse{}
-          .set_header("Content-Type", "text/plain")
-          .set_payload(greeting);
-    });
+    return gcf::HttpResponse{}
+        .set_header("Content-Type", "text/plain")
+        .set_payload(greeting);
+  });
 }
 
 int main(int argc, char* argv[]) {
