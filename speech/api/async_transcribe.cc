d0becb6a00aaf9735d7573f63b3d40e48194e4d6
Carlos O'Ryan <coryan@google.com>
2022-05-20 21:49:09 +0000

    cleanup(speech): use the client library

--- speech/api/async_transcribe.cc
+++ speech/api/async_transcribe.cc
@@ -13,16 +13,11 @@
 // limitations under the License.
 
 #include "parse_arguments.h"
-#include <google/cloud/speech/v1/cloud_speech.grpc.pb.h>
-#include <google/longrunning/operations.grpc.pb.h>
-#include <grpcpp/grpcpp.h>
+#include <google/cloud/speech/speech_client.h>
 #include <iostream>
 #include <string>
-#include <thread>
 
-using google::cloud::speech::v1::LongRunningRecognizeRequest;
-using google::cloud::speech::v1::LongRunningRecognizeResponse;
-using google::cloud::speech::v1::Speech;
+namespace speech = ::google::cloud::speech;
 
 static const char kUsage[] =
     "Usage:\n"
@@ -31,59 +26,33 @@ static const char kUsage[] =
 
 int main(int argc, char** argv) try {
   // [START speech_async_recognize_gcs]
-  // Create a Speech stub connected to the speech service.
-  auto creds = grpc::GoogleDefaultCredentials();
-  auto channel = grpc::CreateChannel("speech.googleapis.com", creds);
-  std::unique_ptr<Speech::Stub> speech(Speech::NewStub(channel));
-  // Create a long operations stub so we can check the progress of the async
-  // request.
-  std::unique_ptr<google::longrunning::Operations::Stub> long_operations(
-      google::longrunning::Operations::NewStub(channel));
+  // Create a Speech client with the default configuration
+  auto client = speech::SpeechClient(speech::MakeSpeechConnection());
   // Parse command line arguments.
   auto args = ParseArguments(argc, argv);
   auto const file_path = args.path;
-  LongRunningRecognizeRequest request;
+  speech::v1::LongRunningRecognizeRequest request;
   *request.mutable_config() = args.config;
 
   // Pass the Google Cloud Storage URI to the request.
   request.mutable_audio()->set_uri(file_path);
   // Call LongRunningRecognize().
-  grpc::ClientContext context;
-  google::longrunning::Operation op;
-  grpc::Status rpc_status =
-      speech->LongRunningRecognize(&context, request, &op);
-  if (!rpc_status.ok()) {
-    // Report the RPC failure.
-    std::cerr << rpc_status.error_message() << std::endl;
-    return -1;
-  }
+  auto future = client.LongRunningRecognize(request);
   // Wait for the operation to complete.  Check the status once per second.
-  std::cout << "Waiting for operation " << op.name() << " to complete.";
-  google::longrunning::GetOperationRequest get_op_request;
-  get_op_request.set_name(op.name());
-  while (!op.done()) {
-    std::cout << "." << std::flush;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    grpc::ClientContext op_context;
-    rpc_status =
-        long_operations->GetOperation(&op_context, get_op_request, &op);
-    if (!rpc_status.ok()) {
-      // Report the RPC failure.
-      std::cerr << rpc_status.error_message() << "\n";
-      return 1;
-    }
+  std::cout << "Waiting for operation to complete ";
+  while (future.wait_for(std::chrono::seconds(1)) ==
+         std::future_status::timeout) {
+    std::cout << '.' << std::flush;
   }
-  std::cout << std::endl;
-  // Unpack the response.
-  if (!op.response().Is<LongRunningRecognizeResponse>()) {
-    std::cerr << "The operation completed, but did not contain a "
-              << "LongRunningRecognizeResponse.\n";
+  std::cout << "DONE\n";
+  auto response = future.get();
+  // If the is an error just report it:
+  if (!response) {
+    std::cerr << "Error in LongRunningRecognize: " << response.status() << "\n";
     return 1;
   }
-  LongRunningRecognizeResponse response;
-  op.response().UnpackTo(&response);
   // Dump the transcript of all the results.
-  for (auto const& result : response.results()) {
+  for (auto const& result : response->results()) {
     for (auto const& alternative : result.alternatives()) {
       std::cout << alternative.confidence() << "\t" << alternative.transcript()
                 << "\n";
